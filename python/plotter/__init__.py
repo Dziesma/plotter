ecd9310007e8513aee2cca38f7610cafed894cde
Gustavs Kehris <gustavs.kehris@cern.ch>
2025-01-29 04:36:53 -0600

    renaming to plotter, setting up __init__.py

---
+++ python/plotter/__init__.py
@@ -0,0 +1,14 @@
+from .plotter import Plotter
+from .region import Region
+from .process import Process
+from .histogram import Histogram, RatioConfig
+from .styles import ErrorBandStyle
+
+__all__ = [
+    "Plotter",
+    "Region",
+    "Process",
+    "Histogram",
+    "RatioConfig",
+    "ErrorBandStyle"
+]
